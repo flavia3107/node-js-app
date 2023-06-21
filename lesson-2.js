98b8a048ed284e97d27e2cc87bdcc8ed373b9d4b
Flavia Kosti <flaviakosti@gmail.com>
2023-06-21 11:15:42 +0200

    Node JS: Lesson 2 - Use expressjs

--- lesson-2.js
+++ lesson-2.js
@@ -3,12 +3,26 @@
  * Run npm init to turn the project into a nodejs managed project,
  * so then 3rd party packages can be installed via the npm install command.
  * The npm init creates the package.json - a configuration file  
+ * 
+ * Express package returns a function and that function returns an object (Stored in the app constant)
+ * 
+ * The app constant is used then to create a server ( with the listen method)
+ * and to handle requests with the get method ( which takes as an argument the path of the request
+ * and the function that should be executed for that request)
+ * 
+ * An app.get method should be defined for each route
  */
 
-const http = require('http');
+const express = require('express');
+const app = express();
 
-function handleRequest(request, response) {
-}
 
-const server = http.createServer(handleRequest);
-server.listen(3000);
+app.get('/currenttime', function (request, response) {
+    response.send('<h1>' + new Date().toISOString() + '</h1>')
+});
+
+app.get('/', function (request, response) {
+    response.send('<h1>Hello World!</h1>')
+});
+
+app.listen(3000);
